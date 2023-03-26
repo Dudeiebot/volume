7a69b89a5b9b750b766a8256573a24689c80aea3
Dudecell <ojoexcel5@gmail.com>
2023-03-26 18:08:25 +0100

    volume upgrade

--- volume.c
+++ volume.c
@@ -1,7 +1,7 @@
 '''
 Modifies the volume of an audio file 
 We are going to do these by reading the input audio file and writing it to a new output file with a factor that determins the volume of the input audio file.
-then these new output ai=udio file is created from a loop that reads the input audio file byte by byte.
+Then these new output audio file is created from a loop that reads the input audio file byte by byte.
 '''
 
 #include <stdint.h>
